0e0c46097dd6fa8db6830fd200b6880d01e89db5
LiamCHayes <liamcharleshayes@gmail.com>
2024-02-20 21:12:02 -0700

    finishing functions to make predictions on when an exercise is being done

--- find_when.R
+++ find_when.R
@@ -149,13 +149,23 @@ getSession <- function(session, exercise) {
 compareExercises <- function(avgExercise, exercise) {
   distances <- rep(0,45)
   for (i in 2:46) {
-    distances[i-1] <- dtw(avgExercise[,i], exercise[,i], k=T, step=typeIIIc)$distance
+    distances[i-1] <- tryCatch(
+      {
+        dtw(avgExercise[,i], exercise[,i], k=T, step=typeIIIc)$distance
+      },
+      error = function(e) {
+        0
+      }
+    ) 
   }
   mean(distances)
 }
 
 # Get data
-avgEx <- getAllAvgExercises(1)
+avgEx1 <- getAllAvgExercises(1)
+avgEx2 <- getAllAvgExercises(2)
+avgEx3 <- getAllAvgExercises(3)
+avgEx <- list(avgEx1, avgEx2, avgEx3)
 s1e1 <- getSession(1, 1) # session 1 exercise 1
 ################################################################################
 
@@ -163,26 +173,232 @@ s1e1 <- getSession(1, 1) # session 1 exercise 1
 
 ## Identify when an exercise is being done
 ################################################################################
-n_dtw_val <- nrow(s1e1) - nrow(avgEx[[1]])
-dtw_val <- rep(0, n_dtw_val+1)
+# Compare session to exercise 1 of all types
+n_dtw_val <- nrow(s1e1) - nrow(avgEx1[[1]])
+dtw_val <- rep(0, nrow(s1e1))
 for (i in 0:n_dtw_val) {
-  window <- s1e1[avgEx[[1]]$time.index + i,]
-  dtw_val[i+1] <- compareExercises(avgEx[[1]], window)
+  window <- s1e1[avgEx1[[1]]$time.index + i,]
+  dtw_val[i+floor(nrow(avgEx1[[1]])/2)] <- compareExercises(avgEx1[[1]], window)
   if (i %% 100 == 0) print(paste(round(i/n_dtw_val*100, 2),'% done', sep=''))
 }
+dtw_val[dtw_val==0] <- dtw_val[dtw_val>0] %>% mean()
+
+n_dtw_val_type2 <- nrow(s1e1) - nrow(avgEx2[[1]])
+dtw_val_type2 <- rep(0, nrow(s1e1))
+for (i in 0:n_dtw_val_type2) {
+  window <- s1e1[avgEx2[[1]]$time.index + i,]
+  dtw_val_type2[i+floor(nrow(avgEx2[[1]])/2)] <- compareExercises(avgEx2[[1]], window)
+  if (i %% 100 == 0) print(paste(round(i/n_dtw_val_type2*100, 2),'% done', sep=''))
+}
+dtw_val_type2[dtw_val_type2==0] <- dtw_val_type2[dtw_val_type2>0] %>% mean()
+
+n_dtw_val_type3 <- nrow(s1e1) - nrow(avgEx3[[1]])
+dtw_val_type3 <- rep(0, nrow(s1e1))
+for (i in 0:n_dtw_val_type3) {
+  window <- s1e1[avgEx3[[1]]$time.index + i,]
+  dtw_val_type3[i+floor(nrow(avgEx3[[1]])/2)] <- compareExercises(avgEx3[[1]], window)
+  if (i %% 100 == 0) print(paste(round(i/n_dtw_val_type3*100, 2),'% done', sep=''))
+}
+dtw_val_type3[dtw_val_type3==0] <- dtw_val_type3[dtw_val_type3>0] %>% mean()
 
 
 s1e1 %>%
-  add_column(dtw_val = c(rep(0, 107),dtw_val,rep(0, 107))) %>%
+  add_column(dtw_val,
+             dtw_val_type2,
+             dtw_val_type3) %>%
   ggplot() +
   geom_line(aes(x=time.index, y=acc_x_u2)) +
-  geom_line(aes(x=time.index, y=dtw_val))
-
-plot(dtw_val)
-
-
+  geom_line(aes(x=time.index, y=dtw_val), col='blue') +
+  geom_line(aes(x=time.index, y=dtw_val_type2), col='red') +
+  geom_line(aes(x=time.index, y=dtw_val_type3), col='green2') 
 
 
+# Compare to all exercises
+dtw_distances <- list()
+for (j in 1:8) {
+  print(paste('Comparing exercise ',j,'/8...', sep=''))
+  
+  # First type
+  n_dtw_val <- nrow(s1e1) - nrow(avgEx1[[j]])
+  dtw_val <- rep(0, nrow(s1e1))
+  for (i in 0:n_dtw_val) {
+    window <- s1e1[avgEx1[[j]]$time.index + i,]
+    dtw_val[i+floor(nrow(avgEx1[[j]])/2)] <- compareExercises(avgEx1[[j]], window)
+  }
+  dtw_val[dtw_val==0] <- dtw_val[dtw_val>0] %>% mean()
+  print('33% done')
+  
+  # Second type
+  n_dtw_val_type2 <- nrow(s1e1) - nrow(avgEx2[[j]])
+  dtw_val_type2 <- rep(0, nrow(s1e1))
+  for (i in 0:n_dtw_val_type2) {
+    window <- s1e1[avgEx2[[j]]$time.index + i,]
+    dtw_val_type2[i+floor(nrow(avgEx2[[j]])/2)] <- compareExercises(avgEx2[[j]], window)
+  }
+  dtw_val_type2[dtw_val_type2==0] <- dtw_val_type2[dtw_val_type2>0] %>% mean()
+  print('66% done')
+  
+  # Third type
+  n_dtw_val_type3 <- nrow(s1e1) - nrow(avgEx3[[j]])
+  dtw_val_type3 <- rep(0, nrow(s1e1))
+  for (i in 0:n_dtw_val_type3) {
+    window <- s1e1[avgEx3[[j]]$time.index + i,]
+    dtw_val_type3[i+floor(nrow(avgEx3[[j]])/2)] <- compareExercises(avgEx3[[j]], window)
+  }
+  dtw_val_type3[dtw_val_type3==0] <- dtw_val_type3[dtw_val_type3>0] %>% mean()
+  print('100% done')
+  
+  df <- data.frame(dtw_val, dtw_val_type2, dtw_val_type3)
+  colnames(df) <- c(paste('dtw_e',j,'_t1',sep=''), 
+                    paste('dtw_e',j,'_t2',sep=''),
+                    paste('dtw_e',j,'_t3',sep=''))
+  dtw_distances <- c(dtw_distances, df)
+}
+dtw_distances <- dtw_distances %>% 
+  as.data.frame() 
+dtw_distances <- dtw_distances %>%
+  add_column(time.index = 1:nrow(dtw_distances)) %>%
+  relocate(time.index) %>%
+  mutate(dtw_1 = select(., starts_with('dtw_e1_')) %>% rowSums(na.rm=T),
+         dtw_2 = select(., starts_with('dtw_e2_')) %>% rowSums(na.rm=T),
+         dtw_3 = select(., starts_with('dtw_e3_')) %>% rowSums(na.rm=T),
+         dtw_4 = select(., starts_with('dtw_e4_')) %>% rowSums(na.rm=T),
+         dtw_5 = select(., starts_with('dtw_e5_')) %>% rowSums(na.rm=T),
+         dtw_6 = select(., starts_with('dtw_e6_')) %>% rowSums(na.rm=T),
+         dtw_7 = select(., starts_with('dtw_e7_')) %>% rowSums(na.rm=T),
+         dtw_8 = select(., starts_with('dtw_e8_')) %>% rowSums(na.rm=T),)
+  
+
+# Plot all dtw_distances
+dtw_distances %>% 
+  ggplot() +
+  geom_line(aes(x=time.index, y=dtw_e1_t1)) +
+  geom_line(aes(x=time.index, y=dtw_e2_t1), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e3_t1), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e4_t1), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e5_t1), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e6_t1), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e7_t1), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e8_t1), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e1_t2)) +
+  geom_line(aes(x=time.index, y=dtw_e2_t2), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e3_t2), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e4_t2), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e5_t2), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e6_t2), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e7_t2), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e8_t2), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e1_t3)) +
+  geom_line(aes(x=time.index, y=dtw_e2_t3), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e3_t3), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e4_t3), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e5_t3), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e6_t3), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e7_t3), col='red') +
+  geom_line(aes(x=time.index, y=dtw_e8_t3), col='red')  +
+  geom_rect(data=data.frame(start=c(90, 330, 550, 900, 1025, 1160, 1470, 1700, 1920), 
+                            end=c(260, 510, 710, 960, 1100, 1230, 1650, 1880, 2100)),
+            aes(xmin=start, xmax=end, ymin=0, ymax=600),
+            col='transparent', fill='green', alpha=0.3) +
+  labs(title = 'All DTW Distances (not summed)')
 
 
+# plot sums of dtw values by exercise
+doing_exercise <- ifelse(dtw_distances$dtw_1 < mean(dtw_distances$dtw_1), 1, 0)
+dtw_distances %>%
+  select(-starts_with('dtw_e')) %>%
+  pivot_longer(cols = starts_with('dtw_'),
+               names_to = 'exercise',
+               names_prefix = 'dtw_', 
+               values_to = 'dtw') %>%
+  ggplot() +
+  geom_line(aes(x=time.index, y=dtw, col=exercise)) +
+  geom_rect(data=data.frame(start=c(90, 330, 550, 900, 1025, 1160, 1470, 1700, 1920), 
+                            end=c(260, 510, 710, 960, 1100, 1230, 1650, 1880, 2100)),
+            aes(xmin=start, xmax=end, ymin=25, ymax=1050),
+            col='transparent', fill='green', alpha=0.2) +
+  geom_line(data=s1e1, aes(x=time.index, y=acc_x_u2), col=doing_exercise+1) +
+  labs(title = 'All DTW Distances (summed)')
+
+
+## Create Functions
+################################################################################
+get_dtw_distances <- function(session, avgEx1, avgEx2, avgEx3) {
+  dtw_distances <- list()
+  for (j in 1:8) {
+    print(paste('Comparing exercise ',j,'/8...', sep=''))
+    
+    # First type
+    n_dtw_val <- nrow(session) - nrow(avgEx1[[j]])
+    dtw_val <- rep(0, nrow(session))
+    for (i in 0:n_dtw_val) {
+      window <- session[avgEx1[[j]]$time.index + i,]
+      dtw_val[i+floor(nrow(avgEx1[[j]])/2)] <- compareExercises(avgEx1[[j]], window)
+    }
+    dtw_val[dtw_val==0] <- dtw_val[dtw_val>0] %>% mean()
+    print('33% done')
+    
+    # Second type
+    n_dtw_val_type2 <- nrow(session) - nrow(avgEx2[[j]])
+    dtw_val_type2 <- rep(0, nrow(session))
+    for (i in 0:n_dtw_val_type2) {
+      window <- session[avgEx2[[j]]$time.index + i,]
+      dtw_val_type2[i+floor(nrow(avgEx2[[j]])/2)] <- compareExercises(avgEx2[[j]], window)
+    }
+    dtw_val_type2[dtw_val_type2==0] <- dtw_val_type2[dtw_val_type2>0] %>% mean()
+    print('66% done')
+    
+    # Third type
+    n_dtw_val_type3 <- nrow(session) - nrow(avgEx3[[j]])
+    dtw_val_type3 <- rep(0, nrow(session))
+    for (i in 0:n_dtw_val_type3) {
+      window <- session[avgEx3[[j]]$time.index + i,]
+      dtw_val_type3[i+floor(nrow(avgEx3[[j]])/2)] <- compareExercises(avgEx3[[j]], window)
+    }
+    dtw_val_type3[dtw_val_type3==0] <- dtw_val_type3[dtw_val_type3>0] %>% mean()
+    print('100% done')
+    
+    df <- data.frame(dtw_val, dtw_val_type2, dtw_val_type3)
+    colnames(df) <- c(paste('dtw_e',j,'_t1',sep=''), 
+                      paste('dtw_e',j,'_t2',sep=''),
+                      paste('dtw_e',j,'_t3',sep=''))
+    dtw_distances <- c(dtw_distances, df)
+  }
+  dtw_distances <- dtw_distances %>% 
+    as.data.frame() 
+  dtw_distances <- dtw_distances %>%
+    add_column(time.index = 1:nrow(dtw_distances)) %>%
+    relocate(time.index) %>%
+    mutate(dtw_1 = select(., starts_with('dtw_e1_')) %>% rowSums(na.rm=T),
+           dtw_2 = select(., starts_with('dtw_e2_')) %>% rowSums(na.rm=T),
+           dtw_3 = select(., starts_with('dtw_e3_')) %>% rowSums(na.rm=T),
+           dtw_4 = select(., starts_with('dtw_e4_')) %>% rowSums(na.rm=T),
+           dtw_5 = select(., starts_with('dtw_e5_')) %>% rowSums(na.rm=T),
+           dtw_6 = select(., starts_with('dtw_e6_')) %>% rowSums(na.rm=T),
+           dtw_7 = select(., starts_with('dtw_e7_')) %>% rowSums(na.rm=T),
+           dtw_8 = select(., starts_with('dtw_e8_')) %>% rowSums(na.rm=T),)
+  dtw_distances
+}
+
+get_exercise_pred <- function(dtw_distances) {
+  dtw_distances %>%
+    select(-starts_with('dtw_e')) %>%
+    sapply(mean) %>%
+    sort(decreasing = F) -> selection
+  selection <- selection[1]
+  
+  dtw_distances %>%
+    select(time.index, 
+           names(selection), 
+           starts_with(paste(strtrim(names(selection), 4),
+                             'e',
+                             substr(names(selection),start=5,stop=5), 
+                             sep=''))) %>%
+    mutate(less_avg = ifelse(.[[2]] < selection, 1, 0)) %>%
+    select(less_avg)
+}
+
+# test functions
+dtw_distances <- get_dtw_distances(s1e1, avgEx1, avgEx2, avgEx3)
+get_exercise_pred(dtw_distances) %>% plot()
 
